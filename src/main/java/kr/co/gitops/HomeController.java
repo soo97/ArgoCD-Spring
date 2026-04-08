f614968d29b87ca2f86c6d3a7fca5c767d5eda95
hyun <syjk7503@gmail.com>
2026-04-08 17:40:00 +0900

    00534

--- src/main/java/kr/co/gitops/HomeController.java
+++ src/main/java/kr/co/gitops/HomeController.java
@@ -3,7 +3,7 @@ package kr.co.gitops;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//
+//ㅇㅇ
 @RestController
 public class HomeController {
 
